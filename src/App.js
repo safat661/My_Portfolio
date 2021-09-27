94f11510c0972c7e7ec79674798272c722c1f5e7
Khan Safat Islam Ananta <safat661@gmail.com>
2021-09-27 17:11:26 -0400

    more changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+
+
+class App extends React.Component {
+  
+  constructor(props){
+
+    super(props);
+    this.state = {
+      title: "Khan Safat Islam Ananta",
+      headerLinks: [
+        {title: "Home", path: "/"},
+        {title: "About", path: "/about"},
+        {title: "Contact", path: "/contact"},
+      ],
+      home: {
+        title: "be relentless",
+        subTitle: "Projects that make a difference",
+        text: "checkout my projects below"
+      },
+      about: {
+        title: "About me"
+      },
+      contact: {
+        title: "Lets's talk"
+      }
+
+    }
+  }
+  
+  render() {
+
+    return (
+      <Router>
+        <Container className='p-0' fluid={true}>
+
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand> Khan Safat Islam Ananta </Navbar.Brand>
+            
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
