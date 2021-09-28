f1b1e97b02aa942fe4db60efd4ac4ce26af2d950
Khan Safat Islam Ananta <safat661@gmail.com>
2021-09-28 00:06:00 -0400

    Almost Done

--- src/App.js
+++ src/App.js
@@ -57,8 +57,8 @@ class App extends React.Component {
           </Navbar>
 
           <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
-          <Route path="/" exact render={() => <AboutPage title={this.state.about.title} /> } />
-          <Route path="/" exact render={() => <ContactPage title={this.state.contact.title}/> } />
+          <Route path="/" render={() => <AboutPage title={this.state.about.title} /> } />
+          <Route path="/" render={() => <ContactPage title={this.state.contact.title}/> } />
 
           <Footer/>
 
